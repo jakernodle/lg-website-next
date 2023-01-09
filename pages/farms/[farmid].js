edebd0c3e4b22651f312ab2387ca945b171c5f88
JohnAnge Kernodle <jakernodle@gmail.com>
2023-01-09 17:25:53 -0500

    details page meta

--- pages/farms/[farmid].js
+++ pages/farms/[farmid].js
@@ -2,6 +2,7 @@ import farmMap from '../../public/data/farmMap';
 import FarmDetail from "../../components/FarmDetail";
 import { StyleRoot } from 'radium';
 import { stepButtonClasses } from '@mui/material';
+import Head from 'next/head';
 
 // This function gets called at build time
 export async function getStaticPaths() {
@@ -38,6 +39,18 @@ function Farm({ farm }){
 
     return (
         <StyleRoot>
+            <Head>
+                <title>{ "See what" + farm.name + "is selling and place an order."}</title>
+                <meta
+                    name="description"
+                    content= {farm.description}
+                    key="desc"
+                />
+                <meta name="geo.placename" content={farm.location} />
+                <meta name="geo.position" content={farm.coordinates.latitude + ";" + farm.coordinates.longitude} />
+                <meta name="geo.region" content="NC" />
+                <meta name="ICBM" content={farm.coordinates.latitude + "," + farm.coordinates.longitude} />
+            </Head>
             <div style={styles.main}>
                 <FarmDetail selectedFarm={farm}/>
             </div>
